b6dc4ca9d670230b9cdd308a8205c8079c959b2f
HicaroD <hdanrlley1@gmail.com>
2022-07-01 15:39:54 -0300

    aboutme: adding a simple README

---
+++ hicaro.py
@@ -0,0 +1,26 @@
+from rich import box
+from rich.columns import Columns
+from rich.console import Console
+from rich.panel import Panel
+from rich.tree import Tree
+
+console = Console(record=True, width=100)
+
+tree = Tree("🤓 [link=websiteportfolio13.herokuapp.com]Hícaro")
+tree.add("Back-end developer")
+
+about = """\
+I'm a back-end developer, currently developing a compiler for my own programming language called [link=github.com/HicaroD/idk]"idk"
+"""
+
+panel = Panel.fit(
+    about, box=box.DOUBLE, border_style="blue", title="[b]Hi there", width=60
+)
+
+console.print(Columns([panel, tree]))
+
+CONSOLE_HTML_FORMAT = """\
+<pre style="font-family:Menlo,'DejaVu Sans Mono',consolas,'Courier New',monospace">{code}</pre>
+"""
+
+console.save_html("README.md", inline_styles=True, code_format=CONSOLE_HTML_FORMAT)
