63c6a27a1ba0fc6f4cfb47262c053475904cc3e7
HicaroD <hdanrlley1@gmail.com>
2023-01-04 12:08:56 -0300

    hicaro: updating my profile intro

--- README.md
+++ README.md
@@ -1,9 +1,19 @@
 <pre style="font-family:Menlo,'DejaVu Sans Mono',consolas,'Courier New',monospace">╔════════════════════════ About me ════════════════════════╗ 🤓 <a href="https://drive.google.com/drive/folders/1HEgd8xXOdbcE1ve6Uhzkxa3vlJ06AfjY?usp=share_link">Hícaro Dânrlley</a>                     
-║ I&#x27;ve been writing code for 3 years in multiple           ║ ├── 🇧🇷 Brazilian                       
-║ programming languages, such as Python, Rust, Go and      ║ ├── 😉 19 years-old                    
-║ Dart. Nowadays, I work as mobile developer with Flutter  ║ ├── 🔧 Back-end developer              
-║ and I have a side project called &#x27;Icarus&#x27;, which is a    ║ ├── 📚 Computer Science student at <a href="https://ufal.br/">UFAL</a>
-║ compiler being written in Rust for my own programming    ║ └── 📇 Contact:                        
-║ language.                                                ║     ├── ✉️: <a href="mailto:hdanrlley1@gmail.com">hdanrlley1@gmail.com</a>        
-╚══════════════════════════════════════════════════════════╝     └── LinkedIn️: <a href="https://www.linkedin.com/in/hicaromiguel/">hicaromiguel</a>         
+║ I&#x27;ve been writing code for 3 years in Python, Go, Rust   ║ ├── 🇧🇷 Brazilian                       
+║ and Dart. Nowadays, I work as Flutter mobile developer   ║ ├── 😉 19 years-old                    
+║ and I really love to work on open source projects. Feel  ║ ├── 🔧 Back-end developer              
+║ free to see some of them below 😄.                       ║ ├── 📚 Computer Science student at <a href="https://ufal.br/">UFAL</a>
+╚══════════════════════════════════════════════════════════╝ └── 📇 Contact:                        
+                                                                 ├── ✉️: <a href="mailto:hdanrlley1@gmail.com">hdanrlley1@gmail.com</a>        
+                                                                 └── LinkedIn️: <a href="https://www.linkedin.com/in/hicaromiguel/">hicaromiguel</a>         
+<span style="font-style: italic">                          Projects                          </span>
+┏━━━━━━━━━━━━━┳━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┳━━━━━━━┓
+┃<span style="font-weight: bold"> Name        </span>┃<span style="font-weight: bold"> Description                        </span>┃<span style="font-weight: bold"> Tools </span>┃
+┡━━━━━━━━━━━━━╇━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━╇━━━━━━━┩
+│ <a href="https://github.com/IcarusLang/Icarus">Icarus</a>      │ An open source programming         │ C++   │
+│             │ language                           │       │
+├─────────────┼────────────────────────────────────┼───────┤
+│ <a href="https://github.com/HicaroD/TodoAsIssue">TodoAsIssue</a> │ From TODOs to issues on GitHub or  │ Dart  │
+│             │ GitLab                             │       │
+└─────────────┴────────────────────────────────────┴───────┘
 </pre>
--- hicaro.py
+++ hicaro.py
@@ -1,31 +1,80 @@
+from pathlib import Path
+import json
+import rich
 from rich import box
 from rich.columns import Columns
 from rich.console import Console
 from rich.panel import Panel
 from rich.tree import Tree
+from rich.table import Table
+from rich.layout import Layout
+
+def get_projects():
+    path = Path("projects.json")
+    if not path.is_file():
+        print("Error: projects.json not found")
+        exit(1)
+
+    with open(path, "r") as file:
+        json_file = json.load(file)
+        return json_file
 
 
-def main():
-    console = Console(record=True, width=100)
+def get_project_table(projects):
+    projects_table = Table(title="Projects", show_header=True, header_style="bold", show_lines=True, width=60)
+    projects_table.add_column("Name")
+    projects_table.add_column("Description")
+    projects_table.add_column("Tools")
 
-    tree = Tree("🤓 [link=https://drive.google.com/drive/folders/1HEgd8xXOdbcE1ve6Uhzkxa3vlJ06AfjY?usp=share_link]Hícaro Dânrlley")
+    for project in projects:
+        project_name_with_link = f"[link={project['link']}]{project['name']}"
+        projects_table.add_row(
+            project_name_with_link,
+            project["description"],
+            project["tools"],
+        )
+
+    return projects_table
+
+
+def get_personal_information_tree():
+    tree = Tree(
+        "🤓 [link=https://drive.google.com/drive/folders/1HEgd8xXOdbcE1ve6Uhzkxa3vlJ06AfjY?usp=share_link]Hícaro Dânrlley"
+    )
     tree.add("🇧🇷 Brazilian")
     tree.add("😉 19 years-old")
     tree.add("🔧 Back-end developer")
     tree.add("📚 Computer Science student at [link=https://ufal.br/]UFAL")
     contact_tree = Tree("📇 Contact:")
     contact_tree.add("✉️: [link=mailto:hdanrlley1@gmail.com]hdanrlley1@gmail.com")
-    contact_tree.add("LinkedIn️: [link=https://www.linkedin.com/in/hicaromiguel/]hicaromiguel")
+    contact_tree.add(
+        "LinkedIn️: [link=https://www.linkedin.com/in/hicaromiguel/]hicaromiguel"
+    )
     tree.add(contact_tree)
+    return tree
 
-    about = "I've been writing code for 3 years in multiple programming languages, such as Python, Rust, Go and Dart. Nowadays, I work as mobile developer with Flutter and I have a side project called 'Icarus', which is a compiler being written in Rust for my own programming language."
 
+def get_about_me_panel():
+    about = """I've been writing code for 3 years in Python, Go, Rust and Dart. Nowadays, I work as Flutter mobile developer and I really love to work on open source projects. Feel free to see some of them below 😄."""
     panel = Panel.fit(about, box=box.DOUBLE, title="About me", width=60)
-    console.print(Columns([panel, tree]))
+    return panel
+
+
+def main():
+    console = Console(record=True, width=100)
+    personal_info_tree = get_personal_information_tree()
+
+    projects = get_projects()
+    print(projects)
+    project_table = get_project_table(projects)
+
+    about_me_panel = get_about_me_panel()
+
+    console.print(Columns([about_me_panel, personal_info_tree]))
+    console.print(project_table)
 
     CONSOLE_HTML_FORMAT = """<pre style="font-family:Menlo,'DejaVu Sans Mono',consolas,'Courier New',monospace">{code}</pre>"""
     console.save_html("README.md", inline_styles=True, code_format=CONSOLE_HTML_FORMAT)
-
 
 if __name__ == "__main__":
     main()
